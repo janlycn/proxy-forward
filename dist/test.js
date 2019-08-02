18a4208d1fba9dccf7a31d53c49ed2e32a680383
janlycn <janlycn@gmail.com>
2019-08-02 10:30:24 +0800

    删除dist

--- dist/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const scheduler_1 = require("./scheduler");
-const s = new scheduler_1.Scheduler();
-s.start();
-//# sourceMappingURL=test.js.map
